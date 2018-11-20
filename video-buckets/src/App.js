9762c69f53018bd260f2eed6359fb88a9dc80732
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-11-19 17:30:21 -0800

    init: first commit of video buckets app

---
+++ video-buckets/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './styles/css/main.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <buttons></buttons>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
