7584b1b6ddb683a203b71312b0ed31ca44bddc47
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-11-19 17:54:45 -0800

    feat: setup of components for app

--- video-buckets/src/App.js
+++ video-buckets/src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './styles/css/main.css';
+import Navigation from './components/navigation';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: {
+        name: '',
+        value: '',
+        type: ''
+      },
+      bucket: {
+        general: [],
+      },
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <buttons></buttons>
-        </header>
+        <Navigation />
+        <Main />
       </div>
     );
   }
