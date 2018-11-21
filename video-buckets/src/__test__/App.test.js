97cbbf29a0ba9d5fd22dacf7cc04c24fd3a472f1
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-11-20 22:32:16 -0800

    fix: add formating to each test

--- video-buckets/src/__test__/App.test.js
+++ video-buckets/src/__test__/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 
-it('renders without crashing', () => {
-  expect.assertions(1);
-  const wrapper = shallow(<App />);
-  expect(wrapper.find('div').text()).toBe('<Navigation /><Buckets />');
+describe('general structure of the app', () => {
+  beforeAll(() => {
+    /* Runs before all tests */
+  })
+  afterAll(() => {
+    /* Runs after all tests */
+  })
+  beforeEach(() => {
+    /* Runs before each test */
+  })
+  afterEach(() => {
+    /* Runs after each test */
+  })
+
+  it('renders without crashing', () => {
+    expect.assertions(1);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div').text()).toBe('<Navigation /><Buckets />');
+  });
 });
