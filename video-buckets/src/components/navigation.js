fb0cd64afd86b2e0c82f4fa908055d11b9ffea29
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-11-21 19:10:01 -0800

    feat: add logic to open correct form based on the button pressed

--- video-buckets/src/components/navigation.js
+++ video-buckets/src/components/navigation.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
-const Navigation = () => {
-    return
-};
+const Navigation = ({ buttonPressed, openVideoBucketEntryForm }) => (
+    <header>
+        <button
+            type='button'
+            data-video={1}
+            onClick={ openVideoBucketEntryForm(event, 'video') }
+        >
+            New Video
+        </button>
+        {
+            buttonPressed === 1 && <Form />
+        }
+        <button
+            type='button'
+            data-bucket={2}
+            onClick={ openVideoBucketEntryForm(event, 'bucket') }
+        >
+            New Bucket
+        </button>
+        {
+            buttonPressed === 2 && <Form />
+        }
+    </header>
+);
+
+
 
 export default Navigation;
