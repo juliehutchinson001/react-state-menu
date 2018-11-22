20f0d23597533127b9c579423b0c31a563a40223
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-11-22 05:49:05 -0800

    feat: finalize create a new entry form

--- video-buckets/src/components/navigation.js
+++ video-buckets/src/components/navigation.js
@@ -1,30 +1,53 @@
 import React from 'react';
 
-const Navigation = ({ buttonPressed, openVideoBucketEntryForm }) => (
-    <header>
-        <button
-            type='button'
-            data-video={1}
-            onClick={ openVideoBucketEntryForm(event, 'video') }
-        >
-            New Video
-        </button>
-        {
-            buttonPressed === 1 && <Form />
-        }
-        <button
-            type='button'
-            data-bucket={2}
-            onClick={ openVideoBucketEntryForm(event, 'bucket') }
-        >
-            New Bucket
-        </button>
-        {
-            buttonPressed === 2 && <Form />
-        }
-    </header>
+const Navigation = ({ buttonPressed, openVideoBucketEntryForm, createNewBucket }) => {
+    return (
+        <header>
+            <button
+                type='button'
+                data-video={ 1 }
+                onClick={ openVideoBucketEntryForm(event, 'video') }
+            >
+                New Video
+            </button>
+            {
+                buttonPressed === 1 && <Form />
+            }
+            <button
+                type='button'
+                data-bucket={ 2 }
+                onClick={ openVideoBucketEntryForm(event, 'bucket') }
+            >
+                New Bucket
+            </button>
+            {
+                buttonPressed === 2
+                && <Form
+                    createNewBucket={ createNewBucket } />
+            }
+        </header>
+    )
+};
+
+const Form = ({ buttonPressed, createNewVideo, createNewBucket }) => (
+    <form onSubmit={ buttonPressed === 1 ? createNewVideo : createNewBucket }>
+        <label> Name
+            <input
+                type='text'
+                required
+                placeholder='Enter a new name'
+                onChange={ }
+            />
+        </label>
+        <label> Description
+            <input
+                type='text'
+                placeholder='Enter a description'
+                onChange={ }
+            />
+        </label>
+        <input type='submit' value='Create' />
+    </form>
 );
-
-
 
 export default Navigation;
