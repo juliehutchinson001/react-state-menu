6cf905e0f89ae9463680a4552d9ebd53342fa175
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-11-23 21:43:04 -0800

    feat: add function to update input values

--- video-buckets/src/App.js
+++ video-buckets/src/App.js
@@ -20,6 +20,8 @@ class App extends Component {
     };
 
     this.openVideoBucketEntryForm = this.openVideoBucketEntryForm.bind(this);
+    // this.createNewBucket = this.createNewBucket.bind(this);
+    // this.createNewVideo = this.createNewVideo.bind(this);
   }
 
   openVideoBucketEntryForm(event, typeOfEntry) {
@@ -34,17 +36,60 @@ class App extends Component {
     this.setState({ button: newEntryFormToBeOpened });
   }
 
+  createNewBucket(event) {
+    event.preventDefault();
+  }
+
+  createNewVideo(event) {
+    event.preventDefault();
+  }
+
+  handleUpdateVideoBucketInputs(event) {
+    const updatedVideoBucketName = event.target.value;
+    const updatedVideoBucketProperty = event.target.name;
+
+    this.setState(oldState => {
+      const newInputValues = Object.assign({}, oldState.input);
+      newInputValues[updatedVideoBucketProperty] = updatedVideoBucketName;
+      return { input: newInputValues };
+    });
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <Navigation
-          openVideoBucketEntryForm={ this.openVideoBucketEntryForm }
-          buttonPressed={ this.state.button }
-        />
-        <Buckets />
-      </div>
+      <AppPresentation
+        openVideoBucketEntryForm={ this.openVideoBucketEntryForm }
+        buttonPressed={ this.state.button }
+        createNewBucket={ event => this.createNewBucket(event) }
+        createNewVideo={ event => this.createNewVideo(event) }
+        handleUpdateVideoBucketInputs={ event => this.handleUpdateVideoBucketInputs(event) }
+      />
     );
   }
+}
+
+const AppPresentation = (props) => {
+  const {
+    openVideoBucketEntryForm,
+    buttonPressed,
+    createNewBucket,
+    createNewVideo,
+    handleUpdateVideoBucketInputs,
+  } = props;
+
+  return (
+    <div className="App">
+      <Navigation
+        openVideoBucketEntryForm={ openVideoBucketEntryForm }
+        buttonPressed={ buttonPressed }
+        createNewBucket={ createNewBucket }
+        createNewVideo={ createNewVideo }
+        handleUpdateVideoBucketInputs={ handleUpdateVideoBucketInputs }
+      />
+      <Buckets />
+    </div>
+  );
 }
 
 export default App;
