c69df103c2ad21632dcbd09e6ef6b57a89bad9ce
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-12-02 10:25:38 -0800

    feat: add test with enzyme

--- video-buckets/src/__test__/App.test.js
+++ video-buckets/src/__test__/App.test.js
@@ -4,12 +4,46 @@ import { shallow } from 'enzyme';
 
 //Components
 import App from '../App';
+import Buckets, { ListOfBuckets } from '../components/buckets';
 
 describe('general structure of the app', () => {
-  it('renders without crashing', () => {
-    expect.assertions(1);
-    const wrapper = shallow(<App />).dive();
-    expect(wrapper.find('div').text())
-      .toBe("<Navigation /><Buckets />");
-  });
+	it('renders App', () => {
+		expect.assertions(1);
+		const wrapper = shallow(<App />);
+		expect(wrapper.find(".App").exists()).toEqual(true);
+
+	});
+
+	it('renders Bucket', () => {
+		expect.assertions(1);
+		const state = {
+			general: {
+				description: '',
+				videos: []
+			},
+		};
+		const wrapper = shallow(<Buckets buckets={state} />);
+		expect(wrapper.find("ListOfBuckets").exists()).toEqual(true);
+	});
+
+	it('renders ListOfBuckets', () => {
+		expect.assertions(1);
+		const buckets = {
+			general: {
+				description: '',
+				videos: []
+			},
+		};
+		const wrapper = shallow(
+			<ListOfBuckets
+				buckets={ buckets }
+				bucketName={ buckets['general'] }
+				bucketArr={ Object.keys(buckets) }
+				videos={ buckets['general'].videos }
+				addVideoToBucket={ () => console.log(buckets) }
+				deleteVideo={ () => console.log('delete') }
+			/>
+		);
+		expect(wrapper.find("div").exists()).toEqual(true);
+	});
 });
